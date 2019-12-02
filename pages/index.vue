1e5e2d38550865abcdae1b3e972aadf03d8ded7d
Prabeen Kumar Soreng <prabeensoreng2119@gmail.com>
2019-12-02 12:26:10 +0530

    landing header added

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="container">
-    <h1>This is landing page</h1>
+    <header class="header">
+      <h1>Header</h1>
+    </header>
+    <div class="landing-header">
+      <div class="landing-header-image"></div>
+      <div class="landing-header-content">
+        <h1 class="header-1">Super Amazing Promo</h1>
+        <h3 class="header-3">Super Amazing Promo Subtitle</h3>
+        <button class="btn btn-primary">Learn More!</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,13 +18,28 @@
 export default {};
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  /* justify-content: center;
-  align-items: center;
-  text-align: center; */
+<style lang="scss">
+.landing-header {
+  width: 100%;
+  height: 350px;
+  &-image {
+    width: 100%;
+    height: 100%;
+    background-image: linear-gradient(
+        to right bottom,
+        rgba(16, 26, 15, 0.8),
+        rgba(107, 107, 107, 0.8)
+      ),
+      url("../static/emile-perron.jpg");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+  &-content {
+    position: absolute;
+    top: 20%;
+    left: 15%;
+    color: white;
+  }
 }
 </style>
