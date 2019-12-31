237c8b8aece95f256e675e6c1571847c9857bef5
Prabeen Kumar Soreng <prabeensoreng2119@gmail.com>
2020-01-01 00:03:35 +0530

    product-hero model created

---
+++ server/models/product-hero.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const productHeroSchema = new Schema({
+  product: { type: Schema.Types.ObjectId, ref: "Product" },
+  image: String,
+  title: String,
+  subtitle: String,
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("ProductHero", productHeroSchema);
